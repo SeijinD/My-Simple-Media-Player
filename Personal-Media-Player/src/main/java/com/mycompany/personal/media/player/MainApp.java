5b732c413166833971e749532eef97f610057639
George Kara <seijindemon@protonmail.com>
2019-12-06 01:25:01 +0200

    add style and images and add many things in new commit

--- Personal-Media-Player/src/main/java/com/mycompany/personal/media/player/MainApp.java
+++ Personal-Media-Player/src/main/java/com/mycompany/personal/media/player/MainApp.java
@@ -1,5 +1,7 @@
 package com.mycompany.personal.media.player;
 
+import java.awt.event.MouseEvent;
+import java.beans.EventHandler;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
@@ -16,7 +18,16 @@ public class MainApp extends Application {
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
-        
+        scene.setMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent doubleClicked)
+            {
+               if(doubleClicked.getClickCount() == 2)
+               {
+                   stage.setFullScreen(true);
+               }
+            }
+        })
         stage.setTitle("Personal Media Player");
         stage.setScene(scene);
         stage.show();
--- Personal-Media-Player/src/main/java/com/mycompany/personal/media/player/MainController.java
+++ Personal-Media-Player/src/main/java/com/mycompany/personal/media/player/MainController.java
@@ -1,21 +1,38 @@
 package com.mycompany.personal.media.player;
 
+import java.awt.event.MouseEvent;
+import java.beans.EventHandler;
 import java.io.File;
 import java.net.URL;
+import java.time.Duration;
 import java.util.ResourceBundle;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 
 public class MainController implements Initializable {
     
-    @FXML
     private MediaPlayer mediaPlayer;
     
     @FXML
-    private Button openfileButton, playButton, pauseButton, stopButton, exitButton;
+    private MediaView mediaView;
+    
+    @FXML
+    private Slider audioSlider, videoSlider;
+    
+    @FXML
+    private Button openfileButton, playButton, pauseButton, stopButton, exitButton, slowButton, slowerButton, fastButton, fasterButton;
        
     private String pathVideo;
     
@@ -27,6 +44,81 @@ public class MainController implements Initializable {
             fileChooser.getExtensionFilters().add(filter);
             File file = fileChooser.showOpenDialog(null);
             pathVideo = file.toURI().toString();
+            
+            if(pathVideo != null)
+            {
+                Media media = new Media(pathVideo);
+                mediaPlayer = new MediaPlayer(media);
+                mediaView.setMediaPlayer(mediaPlayer);              
+                DoubleProperty width = mediaView.fitWidthProperty();
+                DoubleProperty height = mediaView.fitHeightProperty();                    
+                width.bind(Bindings.selectDouble(mediaView.sceneProperty(), "width"));
+                height.bind(Bindings.selectDouble(mediaView.sceneProperty(), "height")); 
+                audioSlider.setValue(mediaPlayer.getVolume() * 100);
+                audioSlider.valueProperty().addListener(new InvalidationListener(){
+                    @Override
+                    public void invalidated(Observable observable)
+                    {
+                        mediaPlayer.setVolume(audioSlider.getValue()/100);
+                    }
+                });
+                mediaPlayer.currentTimeProperty().addListener((InvalidationListener) new ChangeListener<Duration>(){
+                    @Override
+                    public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue)
+                    {
+                        videoSlider.setValue(newValue.toMinutes());
+                    }
+                });
+                videoSlider.setOnMouseClicked(new EventHandler<MouseEvent>(){
+                    @Override
+                    public void handle(MouseEvent event)
+                    {
+                        mediaPlayer.seek(Duration.ofSeconds(videoSlider.getValue()));
+                    }
+                });
+                mediaPlayer.play();
+            }
+    }
+    
+    public void playVideo()
+    {
+        mediaPlayer.play();
+        mediaPlayer.setRate(0);
+    }
+    
+    public void pauseVideo()
+    {
+        mediaPlayer.pause();
+    }
+    
+    public void stopVideo()
+    {
+        mediaPlayer.stop();
+    }
+    
+    public void slowVideo()
+    {
+        mediaPlayer.setRate(.75);
+    }
+    
+    public void slowerVideo()
+    {
+        mediaPlayer.setRate(.5);
+    }
+    
+    public void fastVideo()
+    {
+        mediaPlayer.setRate(1.5);
+    }
+    
+    public void fasterVideo()
+    {
+        mediaPlayer.setRate(2);
+    }
+    
+    public void exit()
+    {
+        System.exit(0);
     }
     
     @Override
